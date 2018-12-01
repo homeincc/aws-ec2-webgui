34197931880c096fbd22c197a4cbbc76f27e339f
Tomáš Votava <tomas.votava@bizztreat.com>
2018-12-01 16:45:11 +0100

    Commit

--- index.php
+++ index.php
@@ -8,8 +8,31 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<script>
+
+function refresh_ec2(() {
+	$.ajax({
+		url: "aws.py",
+		data: {},
+		success: function (e) {alert(e);},
+		dataType: "json"
+	});
+}
+
+
+$(document).ready(function () {
+	refresh_ec2();
+});
+
+</script>
+
 </head>
 <body>
+
+<div class="container" id="ec2-content">
+
+</div>
 
 </body>
 </html>
